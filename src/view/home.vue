f50c8ad60ce00216c1c85d4d5dee2b2e2ad41527
Apache-Ra <summer.vip.20@qq.com>
2020-03-02 01:07:09 +0800

    ···

--- src/view/home.vue
+++ src/view/home.vue
@@ -1,33 +1,16 @@
 <template>
   <div class="home main">
     <div class="videoPlay">
-      <video-player
-        class="video-player vjs-custom-skin"
-        ref="videoPlayer"
-        :playsinline="true"
-        :options="playerOptions"
-      >
-      </video-player>
+      <videoPlayer
+        class="vjs-custom-skin videoPlayer"
+        :options="playerOptions"></videoPlayer>
     </div>
   </div>
 </template>
 <script>
-// 播放器变量
-const options = {
-  bigPlayButton: false,
-  textTrackDisplay: false,
-  posterImage: false,
-  errorDisplay: false,
-  controlBar: false,
-  autoplay: true,
-}
+
 import {mapState} from 'vuex'
-
-import {videoPlayer} from 'vue-video-player'
-import 'videojs-contrib-hls/dist/videojs-contrib-hls'
-import 'video.js/dist/video-js.css'
-import 'vue-video-player/src/custom-theme.css'
-
+import {videoPlayer} from "vue-video-player";
 export default {
   name: "home",
   inject: ['reload'],
@@ -44,20 +27,44 @@ export default {
     return{
       // 视频第一帧
       posterImg:'',
-      playerOptions:{
-        autoplay:true,
-        muted:true,
-        loop:false,
-        preload:'auto',
-        language:'zh-CN',
-        aspectRatio:'16:9',
-        fluid:true,
-        sources: {
-          type:'application/x-mpegURL',
-          src:'http://ivi.bupt.edu.cn/hls/cctv6hd.m3u8'
+      playerOptions: {
+        live: true,
+        autoplay: true, // 如果true，浏览器准备好时开始播放
+        muted: false, // 默认情况下将会消除任何音频
+        loop: false, // 是否视频一结束就重新开始
+        preload: "auto", // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+        aspectRatio: "16:9", // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+        fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+        controlBar: {
+          timeDivider: false,
+          durationDisplay: false,
+          remainingTimeDisplay: false,
+          currentTimeDisplay: true, // 当前时间
+          volumeControl: true, // 声音控制键
+          playToggle: true, // 暂停和播放键
+          progressControl: true, // 进度条
+          fullscreenToggle: true // 全屏按钮
         },
-        width:document.documentElement.clientWidth,
-        notSupportedMessage:'此视频暂无法播放，请稍后再试'
+        height: "300",
+        width: "500",
+        techOrder: ["flash"],
+        flash: {
+          hls: {
+            withCredentials: false
+          },
+          swf: "./static/VideoJS.swf"
+        },
+        sources: [
+          {
+            type: "rtmp/mp4",
+            // src: "rtmp://58.200.131.2:1935/livetv/dftv"
+            src:'rtmp://58.200.131.2:1935/livetv/hunantv'
+          }
+        ],
+
+        autoplay: true,
+        controls: true,
+        notSupportedMessage: "此视频暂无法播放，请稍后再试" // 允许覆盖Video.js无法播放媒体源时显示的默认信息。
       }
     }
   },
